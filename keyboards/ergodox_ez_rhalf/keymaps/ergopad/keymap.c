a74caa1eaed4964656c7b0ed23f1441f14d1f43f
aelius <aeliussaionji@outlook.com>
2019-04-23 21:00:14 -0400

    access other layers, fill out navi, autocolumnize

--- keyboards/ergodox_ez_rhalf/keymaps/ergopad/keymap.c
+++ keyboards/ergodox_ez_rhalf/keymaps/ergopad/keymap.c
@@ -1,61 +1,66 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    SPACE_MOD = SAFE_RANGE
+};
+
 #define LTR0 0 // letters
 #define LTR1 1 // letters
 #define SYMB 2 // symbols
 #define NMPD 3 // numpad
 #define NAVI 4 // navigation
 
+// Tabularize//\*\*/\zs
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LTR0] = LAYOUT_ergodox(
-  KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, RESET,
-  KC_NO,         KC_F,    KC_A,    KC_R,    KC_W,    KC_P,  KC_NO,
-                 KC_O,    KC_E,    KC_H,    KC_T,    KC_D,  KC_TAB,
-  OSM(MOD_LSFT), KC_U,    KC_I,    KC_N,    KC_S,    KC_Y,  KC_ENT,
-                          KC_BSPC, KC_NO,   KC_NO,   KC_NO, KC_NO,
-  KC_NO,   KC_NO,
-  KC_PGUP,
-  KC_PGDN, KC_NO, LT(LTR1, KC_SPC)
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    RESET,
+  KC_NO, /**/             KC_F, /**/     KC_A, /**/     KC_R, /**/     KC_W, /**/     KC_P, /**/     KC_NO,
+  /**/                    KC_O, /**/     KC_E, /**/     KC_H, /**/     KC_T, /**/     KC_D, /**/     KC_TAB,
+  OSM(MOD_LSFT), /**/     KC_U, /**/     KC_I, /**/     KC_N, /**/     KC_S, /**/     KC_Y, /**/     KC_ENT,
+  /**/                    /**/           KC_BSPC, /**/  KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  TG(NMPD), /**/          TG(NAVI),
+  KC_PGUP, /**/
+  KC_PGDN, /**/           KC_SPC, /**/   SPACE_MOD
 ),
 [LTR1] = LAYOUT_ergodox(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_B,    KC_M,    KC_J,    KC_TRNS,
-           KC_Q,    KC_Z,    KC_L,    KC_C,    KC_V,    KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_K,    KC_G,    KC_X,    KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  RGB_TOG, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, RGB_HUD, RGB_HUI
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_B, /**/     KC_M, /**/     KC_J, /**/     KC_NO,
+  /**/                    KC_Q, /**/     KC_Z, /**/     KC_L, /**/     KC_C, /**/     KC_V, /**/     KC_NO,
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_K, /**/     KC_G, /**/     KC_X, /**/     KC_NO,
+  /**/                    /**/           KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  RGB_TOG, /**/           KC_NO,
+  KC_NO, /**/
+  KC_NO, /**/             RGB_HUD, /**/  RGB_HUI
 ),
 [NMPD] = LAYOUT_ergodox(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_ASTR, KC_MINS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_TRNS,
-           KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
-                    KC_0,    KC_DOT,  KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_SLSH, /**/  KC_ASTR, /**/  KC_MINS, /**/  KC_NO,
+  KC_NO, /**/             KC_NO, /**/    KC_7, /**/     KC_8, /**/     KC_9, /**/     KC_PLUS, /**/  KC_NO,
+  /**/                    KC_NO, /**/    KC_4, /**/     KC_5, /**/     KC_6, /**/     KC_NO, /**/    KC_NO,
+  KC_NO, /**/             KC_NO, /**/    KC_1, /**/     KC_2, /**/     KC_3, /**/     KC_NO, /**/    KC_NO,
+  /**/                    /**/           KC_0, /**/     KC_DOT, /**/   KC_NO, /**/    KC_NO, /**/    KC_NO,
+  TG(NMPD), /**/          KC_NO,
+  KC_NO, /**/
+  KC_NO, /**/             KC_NO, /**/    KC_NO
 ),
 [NAVI] = LAYOUT_ergodox(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
+  KC_MUTE, /**/           KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  KC_VOLU, /**/           KC_HOME, /**/  KC_END, /**/   KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  /**/                    KC_PGUP, /**/  KC_LEFT, /**/  KC_DOWN, /**/  KC_UP, /**/    KC_RGHT, /**/  KC_NO,
+  KC_VOLD, /**/           KC_PGDN, /**/  KC_MPRV, /**/  KC_MPLY, /**/  KC_NO, /**/    KC_MNXT, /**/  KC_NO,
+  /**/                    /**/           KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  KC_NO, /**/             TG(NAVI),
+  KC_NO, /**/
+  KC_NO, /**/             KC_NO, /**/    KC_NO
 ),
 [SYMB] = LAYOUT_ergodox(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_QUES, KC_AMPR, KC_ASTR, KC_COLN, KC_SCLN, KC_TRNS,
-           KC_SLSH, KC_EXLM, KC_MINS, KC_AT,   KC_UNDS, KC_TRNS,
-  KC_TRNS, KC_DQUO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
+  KC_NO, /**/             KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  KC_NO, /**/             KC_QUES, /**/  KC_AMPR, /**/  KC_ASTR, /**/  KC_COLN, /**/  KC_SCLN, /**/  KC_NO,
+  /**/                    KC_SLSH, /**/  KC_EXLM, /**/  KC_MINS, /**/  KC_AT, /**/    KC_UNDS, /**/  KC_NO,
+  KC_NO, /**/             KC_DQUO, /**/  KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  /**/                    /**/           KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO, /**/    KC_NO,
+  KC_NO, /**/             KC_NO,
+  KC_NO, /**/
+  KC_NO, /**/             KC_NO, /**/    KC_NO
 ),
 };
 
@@ -65,6 +70,27 @@ void matrix_init_user(void) {
   rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
 #endif
 };
+
+static uint16_t key_check;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+		case SPACE_MOD:
+			if (record->event.pressed) {
+				key_check = 1;
+				layer_on(LTR1);
+			}
+			else {
+				layer_off(LTR1);
+				if (key_check == 1) {
+					tap_code(KC_SPC);
+				}
+			}
+			return false;
+		default:
+			key_check = 0;
+			return true;
+	}
+}
 
 // Runs whenever there is a layer state change.
 uint32_t layer_state_set_user(uint32_t state) {
